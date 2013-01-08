0c64534bab2bb1c51b3ecf87f6e713ca012474c9
Dario Malchiodi <malchiodi@di.unimi.it>
2013-01-08 20:55:56 +0100

    Added license and copyright statements

--- xmlRPC.m
+++ xmlRPC.m
@@ -1,5 +1,23 @@
 (* ::Package:: *)
 
+
+(******************************************************************************
+       Copyright (C) 2010 Dario Malchiodi <malchiodi@di.unimi.it>
+
+This file is part of xmlRPC.
+xmlRPC is free software; you can redistribute it and/or modify it under the
+terms of the GNU Lesser General Public License as published by the Free
+Software Foundation; either version 2.1 of the License, or (at your option)
+any later version.
+xmlRPC is distributed in the hope that it will be useful, but without any
+warranty; without even the implied warranty of merchantability or fitness
+for a particular purpose. See the GNU Lesser General Public License for
+more details.
+You should have received a copy of the GNU Lesser General Public License
+along with xmlRPC; if not, see <http://www.gnu.org/licenses/>.
+
+******************************************************************************)
+
 (* : Title : xmlRPC *)
 (* : Context : xmlRPC` *)
 (* : 
